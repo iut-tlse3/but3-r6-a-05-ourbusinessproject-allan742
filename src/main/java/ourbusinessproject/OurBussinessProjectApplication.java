2a74a2710a3c637ba89cbf8781bc12f4ba42bd21
Allan <allan.forwork9@gmail.com>
2026-01-27 09:43:56 +0100

    Initial commit

---
+++ src/main/java/ourbusinessproject/OurBussinessProjectApplication.java
@@ -0,0 +1,13 @@
+package ourbusinessproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurBussinessProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurBussinessProjectApplication.class, args);
+    }
+
+}
