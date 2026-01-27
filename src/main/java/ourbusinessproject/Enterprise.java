744e4010b9f24a95e5f663a4cd3c83b78c439fad
Allan <allan.forwork9@gmail.com>
2026-01-27 11:52:24 +0100

    fix #1.3 Mapping Project et Enterprise

--- src/main/java/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -1,10 +1,17 @@
 package ourbusinessproject;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Enterprise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @NotBlank
     private String Name;
     @Size(min = 10)
--- src/main/java/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/Project.java
@@ -1,9 +1,14 @@
 package ourbusinessproject;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 
-
+@Entity
 public class Project {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     @NotBlank
     private String title;
     private String description;
